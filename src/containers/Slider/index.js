1f0ae4c1527c9149fc5a615d1fe57a433a98f5dc
mmcc91 <121430505+mmcc91@users.noreply.github.com>
2024-05-09 15:03:52 +0200

    test ok soutenance ok

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,7 +7,7 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus?.sort((evtB, evtA) => // trie decroissant par date
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtB.date) < new Date(evtA.date) ? -1 : 1
   );
 
   const timeoutRef = useRef(null); // ajout de useRef pour le timer
